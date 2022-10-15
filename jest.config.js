2809216a9f789b2ce2296c8c63a07405bb3af1a2
Phat Nguyen <phatnguyentan0491@gmail.com>
2022-10-15 18:08:56 +0700

    first commit

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    globals: {
+        'ts-jest': {
+            tsConfig: 'tsconfig.json'
+        }
+    },
+    moduleFileExtensions: [
+        'ts',
+        'js'
+    ],
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest'
+    },
+    testMatch: [
+        '**/test/**/*.test.(ts|js)'
+    ],
+    testEnvironment: 'node',
+    testResultsProcessor: 'jest-sonar-reporter',
+    coverageDirectory: 'output/coverage/jest',
+    reporters: ['default', 'jest-junit'],
+};
---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import { Request, Response } from 'express';
+import compression from 'compression';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/env', (req: Request, res: Response) => {
+    res.json(process.env);
+});
+
+app.get('/env1', (req: Request, res: Response) => {
+    res.json(process.env);
+});
+
+app.get('/health', (req: Request, res: Response) => {
+    res.json({'health': 'OK'});
+});
+
+export default app;
